62db8e3c3f10cb12d6fe939cdeeb736870032ce2
swatineha2312 <swatineha2312@gmail.com>
2022-01-18 14:46:26 +1100

    Add project files.

---
+++ Areas/Hospital/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorFinder.Areas.Hospital.Controllers
+{
+    [Area("Hospital")]
+    public class HomeController : Controller
+    {
+        public IActionResult AddDoctorListing()
+        {
+            return View();
+        }
+        public IActionResult Doctor()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult HospitalLogin()
+        {
+            return View();
+        }
+    }
+}
